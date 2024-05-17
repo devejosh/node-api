5bb22ea56835f1fdf6e2f848813defb1dbd2d101
= <=>
2024-05-18 03:07:40 +0530

    added configuration file

--- config/config.js
+++ config/config.js
@@ -1,3 +1,12 @@
 // Require the environment variables
 require ('dotenv').config()
 
+module.exports = {
+    spotify: {
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+    },
+    jwtSecret : process.env.JWT_SECRET,
+
+};
