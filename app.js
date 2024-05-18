c29634cfa927e466923be2ad8b225b8efc40ebc9
= <=>
2024-05-18 22:01:35 +0530

    included the node modules in gitignore

--- app.js
+++ app.js
@@ -14,7 +14,3 @@ app.get ('/' , (req, resp) => {
 const server = app.listen(PORT , () => {
     console.log (`Express is listening on port : ${PORT}`)
 })
-
-server.close(() => {
-    console.log ("Server instance has ended")
-})
